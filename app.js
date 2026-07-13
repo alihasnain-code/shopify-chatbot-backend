42e4b8c5e0f11bf9c1804c911dfaeee7e225dfc9
Ali Hasnain <ali.hasnain.official@outlook.com>
2026-07-13 21:52:17 +0500

    new routes are added

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import chatRouter from './routes/chat.route.js'
+import historyRouter from './routes/history.route.js'
+import cartRouter from './routes/cart.route.js'
 
 const app = express()
 
@@ -7,5 +9,7 @@ app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/api/v1', chatRouter)
+app.use('/api/v1', historyRouter)
+app.use('/api/v1', cartRouter)
 
 export default app
